86ce61cd2d6eb2b1e25f63c54077b0d2be759954
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-18 13:01:58 -0600

    Add state to App, pass ideas props to Ideas and Card components

--- src/Ideas.js
+++ src/Ideas.js
@@ -2,9 +2,21 @@ import React from "react"
 import "./Ideas.css"
 import Card from "./Card"
 
-const Ideas = () => {
+const Ideas = ( {ideas} ) => {
+
+	const ideaCards = ideas.map(idea => {
+		return (
+			<Card
+				id={idea.id}
+				title={idea.title}
+				description={idea.description}
+				key={idea.id}
+			/>
+		)
+	})
+
 	return (
-		<div className="Ideas-container"><Card /></div>
+		<div className="Ideas-container">{ideaCards}</div>
 	)
 }
 

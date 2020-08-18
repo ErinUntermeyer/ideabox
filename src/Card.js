137a8ebcbf4d998030435c55cd8e2ad52f921c6f
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-18 11:11:30 -0600

    Add Ideas and Card components with styling

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Card.css"
+
+const Card = () => {
+	return (
+		<div className="Card">
+			<h3>Card!</h3>
+		</div>
+	)
+}
+
+export default Card
---
+++ src/Ideas.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Ideas.css"
+import Card from "./Card"
+
+const Ideas = () => {
+	return (
+		<div className="Ideas-container"><Card /></div>
+	)
+}
+
+export default Ideas
